cf24bd91b6a515b7aac85d42bfbfff36f025d76b
Tim Gifford <giffordtim@johndeere.com>
2017-07-11 22:17:51 -0500

    Output the metadata from a single image

---
+++ ImageMetadataExtractor/Program.cs
@@ -0,0 +1,30 @@
+﻿using MetadataExtractor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMetadataExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var image = File.OpenRead(@"images\smith-river.JPG");
+            var directories = ImageMetadataReader.ReadMetadata(image);
+
+            foreach( var dir in directories)
+            {
+                foreach (Tag tag in dir.Tags)
+                {
+                    Console.WriteLine(tag);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
